4c2c0faceaed4652c10b2343185e45f46d6afb3e
Jorge Rimblas <jorge@rimblas.com>
2023-04-24 21:12:37 -0500

    reset release

--- release/_release.sql
+++ release/_release.sql
@@ -2,27 +2,16 @@
 
 PRO TABLES
 PRO ----------------------------------------
-PRO .. wmg_parameters
-@../install/wmg_parameters.sql
 
 
 PRO VIEWS
 PRO ----------------------------------------
-PRO .. wmg_tournament_sessions_v
-@../views/wmg_tournament_sessions_v.sql
 
 
 PRO PACKAGES
 PRO ----------------------------------------
-PRO .. wmg_util.pks
-@../packages/wmg_util.pks
 
-PRO .. wmg_util.pkb
-@../packages/wmg_util.pkb
 
 PRO DML
 PRO ----------------------------------------
-PRO .. cleanup old tables
-drop table wmg_course_previews2 purge;
-drop table wmg_course_previews_err$ purge;
 
