035fe9b02c3001b1d0d01a107e7e1822496c975c
Jorge Rimblas <jorge@rimblas.com>
2023-05-07 21:12:49 -0500

    prep new release

--- release/_release.sql
+++ release/_release.sql
@@ -3,25 +3,21 @@ set feedback off
 set verify off
 
 
--- PRO APEX DISABLE APP
--- PRO ________________________________________
+PRO APEX DISABLE APP
+PRO ________________________________________
 
--- set define &
--- @../apex/apex_disable 200
--- set define off
+set define &
+@../apex/apex_disable 200
+set define off
 
 PRO TABLES
 PRO ________________________________________
-alter table wmg_tournament_players add no_show_flag                  varchar2(1);
-comment on column wmg_tournament_players.no_show_flag is 'Flag players that do not show up';
 
 
 
 PRO VIEWS
 PRO ________________________________________
 
-PRO .. wmg_tournament_player_v
-@../views/wmg_tournament_player_v.sql
 
 
 PRO PACKAGES
@@ -41,6 +37,6 @@ PRO ________________________________________
 
 PRO APEX 
 PRO ________________________________________
--- @../apex/pre_apex_install.sql
--- @../apex/f200.sql
+@../apex/pre_apex_install.sql
+@../apex/f200.sql
 
