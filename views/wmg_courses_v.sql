43bf6d57bad1c7f6079ee456f852255423966e3e
Jorge Rimblas <jorge@rimblas.com>
2023-05-12 00:44:03 -0500

    Added Utopia and Reaistic scores

--- views/wmg_courses_v.sql
+++ views/wmg_courses_v.sql
@@ -1,5 +1,19 @@
 create or replace view wmg_courses_v 
 as
+with w_course_best as (
+select course_id
+     -- , sum(s) best_strokes
+     , sum(min_par) best_under
+  from (
+    select course_id
+         , h
+         -- , min(score)  s
+         , min(par) min_par
+    from wmg_rounds_unpivot_mv
+   group by course_id,  h
+   )
+ group by course_id
+)
 select 
     c.id                                         course_id,
     c.release_order,
@@ -27,9 +41,10 @@ select
     cs.h17,
     cs.h18,
     cs.h1+ cs.h2+ cs.h3+ cs.h4+ cs.h5+ cs.h6+ cs.h7+ cs.h8+ cs.h9+ cs.h10+ cs.h11+ cs.h12+ cs.h13+ cs.h14+ cs.h15+ cs.h16+ cs.h17+ cs.h18 course_par
-from 
-    wmg_courses c,
-    wmg_course_strokes cs
-where
-    cs.course_id(+) = c.id
+  , cb.best_under
+from wmg_courses c
+   , wmg_course_strokes cs
+   , w_course_best cb
+where cs.course_id(+) = c.id
+  and cb.course_id(+) = c.id
 /
