36ef2838eb3599d2bedc4131e4122837f5a1e8d4
Jorge Rimblas <jorge@rimblas.com>
2024-03-09 13:19:29 -0600

    Load current courses

---
+++ data/seed_wmg_courses.sql
@@ -0,0 +1,110 @@
+REM INSERTING into WMG_COURSES
+SET DEFINE OFF;
+SET SQLBLANKLINES ON
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ZZH','Temple at Zerzura - Hard','Zerzura_CatUrn','This course specifically used some new tech, that would then be used for Journey Hard''s flashlight system, in order to create a hidden ""blacklight"" foxhunt effect. This was also used to show some realtime light effects on the environment for a special Hole 18 effect.
+
+-Chase :MC:','H',17,to_timestamp('20-APR-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('WWH','Widow’s Walkabout - Hard','WidowsWalk_ClownDoll','😱 **__WIDOW''S WALKABOUT DESIGN:__**
+The Widow''s Walkabout environment is a love letter to all of our favorite Victorian Gothic depictions of eerie haunted houses. From Guillermo del Toro to Disney''s Haunted Mansion, with additional inspirtation from the Addam''s Family and Series of Unfortunate Events, we pulled together all of our favorite haunted abodes and created this tribute to all things spooky, kooky, and ooky.
+
+Widow''s Walkabout Hard is our love letter to vintage noir haunted house films of the past like The Haunting (1963) and The House on Haunted Hill (1959) with just a touch of Addam''s Family and the Munsters sprinkled in.
+-**Don** _MC_','H',22,to_timestamp('19-OCT-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('MWE','Meow Wolf','noButt_happy',null,'E',23,to_timestamp('07-DEC-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('AWH','Around The World - Hard','AroundWorld_Elephant',null,'H',24,to_timestamp('18-JAN-2024 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('EDH','El Dorado Hard','ElDorado_Conquistador','Players talk about how hard this course is, but what they may not realize is that holes often start out much more difficult then get refined both for competitive and casual play throughout the design and testing process. 
+
+-**David** _MC_','H',15,to_timestamp('02-JUN-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('BBH','Bogey''s Bonanza Hard','Bogeys_Cowskull',null,'H',7,to_timestamp('10-JUN-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('QVH','Quixote Valley Hard','Quixote_Spuds',null,'H',8,to_timestamp('07-OCT-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SLH','Shangri-La Hard','ShangriLa_Lantern','The lanterns were just an idea, but after we saw how much they added to the level, we re-dressed the easy version of the course as if it were in preparation for this evening festival.
+
+-**Lucas** _MC_','H',14,to_timestamp('16-DEC-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SWE','Sweetopia','Sweetopia_Bear','We had a sugar sparkle shader that made things quite pretty.
+We would''ve had to commit to everything being sparkly, and that was too much of a good thing.  Plus performance took a hit, and NOBODY wants that.
+
+_-(Henning) :MC:_','E',9,to_timestamp('17-FEB-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('TSE','Tethys Station','Tethys_GolferSpecimen',null,'E',6,to_timestamp('03-MAR-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SSE','Seagull Stacks','SeagullStacks_Sword',null,'E',3,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('AME','Arizona Modern','ArizonaModern_Cactus',null,'E',4,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('OGH','Original Gothic Hard','OriginalGothic_Statue',null,'H',5,to_timestamp('27-DEC-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('LBE','Labyrinth','Labyrinth_Worm','The stink ball was a heavily debated topic among the team. While everyone was for it, we weren''t sure if it was too crude to include. Ultimately we decided it wouldn''t be the bog of eternal stench without the consequences of the bog of eternal stench.
+ 
+-**Shane** _MC_','E',10,to_timestamp('28-JUL-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('AMH','Arizona Modern Hard','ArizonaModern_Cactus',null,'H',4,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('MYE','Myst','Myst_Book',null,'E',11,to_timestamp('15-NOV-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ATE','Atlantis','Atlantis_Crab','Have you tried riding one of the sealife (the whale, turtle, and sharks)? They will take you on a round-trip swimming tour of their colorful world.  Except the Manta rays, they will shrink under your feet as they slowly carry you into the void... only to be reborn where they started again!
+
+-**Liz** _MC_','E',16,to_timestamp('26-JAN-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SWH','Sweetopia Hard','Sweetopia_Bear','The iconic hard mode music track by Chris Reyman was a last minute addition, done in a couple hours just days before launch. It had previously been the same music as easy.
+
+-**Lucas** - _MC_','H',9,to_timestamp('17-FEB-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('QVE','Quixote Valley','Quixote_Spuds',null,'E',8,to_timestamp('30-SEP-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SSH','Seagull Stacks - Hard','SeagullStacks_Sword',null,'H',3,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('GBE','Gardens of Babylon','GardensBabylon_Plant','We tried several more realistic versions of the folliage, but ultimately decided that the low poly looked better with our art style. Hidden in the level you can find a leafless tree, which is a memorial to all the great work that we didn''t use.
+
+_-Lucas :MC:_','E',13,to_timestamp('18-NOV-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('BBE','Bogey''s Bonanza','Bogeys_Cowskull',null,'E',7,to_timestamp('10-JUN-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('LBH','Labyrinth - Hard','Labyrinth_Worm','After struggling to capture the ballroom sequence as a single hole, hard mode was envisioned as if the masquerade was spilling out into the entire environment.
+-**Lucas** _MC_','H',10,to_timestamp('28-JUL-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('CBH','Cherry Blossom Hard','CherryBlossom_Origami',null,'H',2,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('OGE','Original Gothic','OriginalGothic_Statue',null,'E',5,to_timestamp('08-NOV-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('20H','20,000 Leagues Hard','20K_Bathosphere','Early drafts of this course called for "the masterpiece containing masterpieces" to be berthed in an undersea cave.  But alas……
+
+_-(Henning) :MC: _','H',18,to_timestamp('29-SEP-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('TTE','Tourist Trap','TouristTrap_Bottle',null,'E',1,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('TSH','Tethys Station Hard','Tethys_GolferSpecimen',null,'H',6,to_timestamp('03-MAR-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('CBE','Cherry Blossom','CherryBlossom_Origami',null,'E',2,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('EDE','El Dorado','ElDorado_Conquistador','Originally, there were more gold objects spread around the course to hint at the large gold stash, but we decided to make the gold of this lost city a true mystery.
+
+_-(Ryan) :MC:_','E',15,to_timestamp('02-JUN-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('TTH','Tourist Trap Hard','TouristTrap_Bottle',null,'H',1,to_timestamp('24-SEP-2020 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('SLE','Shangri-La','ShangriLa_Lantern','The cable car ride was a very late addition. Originally the crossing between holes 9 and 10 was just a really long rope bridge.
+
+-**Henning** - _MC_','E',14,to_timestamp('16-DEC-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('GBH','Gardens of Babylon Hard','GardensBabylon_Plant','There are about 3,000 little houses in the background of Babylon, within a 7 km area. You can explore as far as you want, and quite a few are lit up at night!
+
+**-Henning** _MC_','H',13,to_timestamp('18-NOV-2021 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('20E','20,000 Leagues Under The Sea','20K_Bathosphere','How we arrive onto the course was a major design challenge, as we really wanted to give players an opportunity to see the outside first to understand where you were.
+This was the first course featuring the work of Don Carson, whose initial cutaway sketch very closely resembles the final course.
+
+**-Lucas** _MC_','E',18,to_timestamp('29-SEP-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('MYH','Myst Hard','Myst_Book','Hole 17 at one point had the burned remains of Atrus''s library books as a hazard spread around the green of the hole, but then we changed course to the hole you know today. 
+
+-**Ryan** _MC_','H',11,to_timestamp('15-NOV-2022 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('JCE','Journey to the Center of the Earth','Journey_Mushroom','The rising and falling water levels (especially combined with the low light conditions in the cave were a new set of obstacles for the game and while something that''s a classic mini golf trope, we wanted to take it a little further especially so that the risk vs. reward was heightened for our competitive players. 
+
+-**David** _MC_','E',19,to_timestamp('08-JUN-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('MWH','Meow Wolf - Hard','noButt_happy',null,'H',23,to_timestamp('07-DEC-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('UTE','Upside Town','UpsideTown_PizzaRat','There''s probably more hidden easter eggs in this one course than any other, but since they’re literally in every direction, many have missed them. Many of our competitive players bristle at this course because it’s outside their comfort zone, but we think of it as a great (and fun) equalizer.
+
+-**Chase** _MC_','E',12,to_timestamp('09-MAR-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('UTH','Upside Town - Hard','UpsideTown_PizzaRat',null,'H',12,to_timestamp('09-MAR-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('LLE','Laser Lair','LaserLair_Serverbot','Hole 13 in particular was vexxing during the internal testing process, because while it is meant to be evil (it''s right in the title), it started out nearly impossible between the lasers and the angles. But as with even our most daunting holes, with refinements and practice, we found a good balance in the end.
+
+-**David** _MC_','E',20,to_timestamp('20-JUL-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('WWE','Widow’s Walkabout','WidowsWalk_ClownDoll','😱 **__WIDOW''S WALKABOUT DESIGN:__**
+The Widow''s Walkabout environment is a love letter to all of our favorite Victorian Gothic depictions of eerie haunted houses. From Guillermo del Toro to Disney''s Haunted Mansion, with additional inspirtation from the Addam''s Family and Series of Unfortunate Events, we pulled together all of our favorite haunted abodes and created this tribute to all things spooky, kooky, and ooky.
+
+What design team doesn''t want to design thier own haunted manor house. Widow''s Walkabout Easy is meant to represent that old house everyone gets dared to spend the night in but few are brave enough to enter. We chose to minimize the number of ghosts our players will encounter allowing their imaginition to fill in the blanks when it comes to the spirit realm.
+
+-**Don** _MC_','E',22,to_timestamp('19-OCT-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('AWE','Around The World in 80 Days','AroundWorld_Elephant',null,'E',24,to_timestamp('18-JAN-2024 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ILE','Ice Lair','IceLair_Puffin',null,'E',25,to_timestamp('07-MAR-2024 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ATH','Atlantis Hard','Atlantis_Crab','While the team decided underwater physics would make for unreliable and not-fun mini golf, we worked to create underwater immersion with the many fish (which are threaded and Unity Burst enhanced) to get the most out of them while not interfering with course performance or putting sight lines.
+
+_-(Scott) :MC: _','H',16,to_timestamp('26-JAN-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ZZE','Temple at Zerzura','Zerzura_CatUrn','This was essentially our first PSVR2 release so a lot of work went in behind the scenes to get crossplay and PlayStation guidelines in place. Was a big relief when things went off mostly without a hitch (mostly due to how great JP is). It also got us talking about how we want to do particle fx in the future, more texture based or geo based.
+
+-**Scott** _MC_','E',17,to_timestamp('20-APR-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('JCH','Journey to the Center of the Earth - Hard','Journey_Mushroom','This course leveraged a new lighting system in order to sell the idea of flashlights in a dark cave. This flashlight system didn’t use realtime lights but instead had lights in the shader that were then updated in realtime by the players in the scene. As well, a special RGB lighting setup needed to be used in order to fully leverage the effect. The level was lit only using Red, Green, and Blue lights that then when combined with the shader, produced the colors of the scene and allowed the flashlight to appear where we intended.
+
+-**Chase** _MC_','H',19,to_timestamp('08-JUN-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('LLH','Laser Lair - Hard','LaserLair_Serverbot','Originally we planned to have life size sharks with lazzzzer beams attached to their heads in the pool for hard mode to play off the little shark floaty in easy mode. While they didn''t make the final cut, we comprimised by giving our little floaty shark it''s own little laser beam attached to it''s head.
+
+**-Shane** _MC_','H',20,to_timestamp('20-JUL-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ALE','Alfheim','Alfheim_Beehive','Where a normal course takes about 9 months from conception to release, this was in and out of production for 2 1/2 years!
+-**Henning** _MC_','E',21,to_timestamp('07-SEP-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ALH','Alfheim Hard','Alfheim_Beehive','This course is all about the curves, and how you can spend a literal eternity perfecting your shots. Holes 6 and 11 are good examples of the balance of speed, momentum, distance, and all of the hole design elements that make the course enjoyable but also challenging enough to warrant repeated play and worth developing one''s shots.
+
+-**David** _MC_','H',21,to_timestamp('07-SEP-2023 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
+Insert into WMG_COURSES (CODE,NAME,COURSE_EMOJI,FACTOIDS,COURSE_MODE,RELEASE_ORDER,RELEASE_DATE) values ('ILH','Ice Lair - Hard','IceLair_Puffin',null,'H',25,to_timestamp('07-MAR-2024 12:00:00 AM','DD-MON-RR HH.MI.SSXFF AM'));
