0be603aa93d0a42de850ffdd7c30b1bcfa2cfdb4
Jorge Rimblas <jorge@rimblas.com>
2024-03-09 11:52:16 -0600

    Added missing tables to install

--- install/_ins.sql
+++ install/_ins.sql
@@ -5,6 +5,10 @@ PRO .. wmg_constraint_lookup.sql
 @wmg_constraint_lookup.sql
 PRO .. wmg_courses.sql
 @wmg_courses.sql
+PRO .. wmg_course_previews.sql
+@wmg_course_previews.sql
+PRO .. wmg_course_strokes.sql
+@wmg_course_strokes.sql
 PRO .. wmg_badge_types.sql
 @wmg_badge_types.sql
 PRO .. wmg_issues.sql
@@ -27,17 +31,15 @@ PRO .. wmg_verification_queue.sql
 @wmg_verification_queue.sql
 PRO .. wmg_rounds.sql
 @wmg_rounds.sql
-
 PRO .. wmg_player_badges.sql
 @wmg_player_badges.sql
 PRO .. wmg_player_unicorns.sql
 @wmg_player_unicorns.sql
-
 PRO .. wmg_messages.sql
 @wmg_messages.sql
-PRO .. wmg_notification_subs.sql
-@wmg_notification_subs.sql
 PRO .. wmg_notification_types.sql
 @wmg_notification_types.sql
+PRO .. wmg_notification_subs.sql
+@wmg_notification_subs.sql
 PRO .. wmg_webhooks.sql
 @wmg_webhooks.sql
