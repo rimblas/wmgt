4efffd0a9746944e5b4d8f5e1ba182f810d2d4dd
Jorge Rimblas <jorge@rimblas.com>
2024-09-15 15:34:38 -0500

    new KW constraint errors

--- data/seed_constraint_lookup.sql
+++ data/seed_constraint_lookup.sql
@@ -11,3 +11,5 @@ insert into wmg_constraint_lookup (constraint_name, message) values ('WMG_PLAYER
 
 insert into wmg_constraint_lookup (constraint_name, message) values ('WMG_COURSE_PREVIEWS_U01', 'Hole preview already exists.');
 insert into wmg_constraint_lookup (constraint_name, message) values ('WMG_TOURNAMENT_ROOM_TEMPLATE_U1', 'Room No already exists.');
+insert into wmg_constraint_lookup (constraint_name, message) values ('KW_MONTHLY_PLAYERS_U01', 'Player already exists for this month.');
+insert into wmg_constraint_lookup (constraint_name, message) values ('KW_MONTHLY_RESULT_PLAYERS_FK', 'The player already has scores for this month.');
