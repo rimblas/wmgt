18d2d0e753bfb7a015be228cebbbe3148a459a65
Jorge Rimblas <jorge@rimblas.com>
2025-03-02 11:33:55 -0600

    Room Teamplates

--- sql/populate_new_room_templates.sql
+++ sql/populate_new_room_templates.sql
@@ -1,3 +1,10 @@
+select tt.tournament_id, t.name, count(*)
+from wmg_tournament_room_template tt
+  join wmg_tournaments t on t.id = tt.tournament_id
+group by tt.tournament_id, t.name
+/
+
+
 insert into wmg_tournament_room_template (
     tournament_id
   , room_no
@@ -5,10 +12,11 @@ insert into wmg_tournament_room_template (
   , day_offset
   , time_slot
 )
-select 243 tournament_id
+select 243 tournament_id -- new season
   , room_no
   , extra_rooms
   , day_offset
   , time_slot
 from wmg_tournament_room_template
-where tournament_id = 203
+where tournament_id = 203  -- prev season
+/
